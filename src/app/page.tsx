69483db421c0324f213dd1b35327bdfc83f71c33
Amber Asif <muhammadasif.qict@gmail.com>
2024-12-16 22:24:59 +0500

    error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 
 import Hero from "./Components/Hero";
-import CompanyLogo from "./Components/CompanyLogo";
-import FeatureProduct from "./Components/FeatureProduct";
-// import Cart from "./Components/Cart";
+import CompanyLogo from "./Components/CompanyLogo"
+
 export default function Home() {
   return (
    <>
     <Hero/>
   <CompanyLogo/>
-   <FeatureProduct/>
-   {/* <Cart/> */}
+   
+  
   </>
 
   );
